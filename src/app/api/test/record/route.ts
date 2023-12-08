bd281da9a6633f44b16c8b0d6b95be3db07b1881
sionel <sadb0101@naver.com>
2023-12-08 10:40:04 +0900

    Fix : cors 수정

--- src/app/api/test/record/route.ts
+++ src/app/api/test/record/route.ts
@@ -11,15 +11,34 @@ export async function POST(request: NextRequest) {
   logger.info(`이름 : ${name}`);
 
   if (typeof score !== "number") {
-    return NextResponse.json("서식이 잘못되었습니다", { status: 200 });
+    return NextResponse.json("서식이 잘못되었습니다", {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
   }
   score = clientScore > 100 ? clientScore % 100 : clientScore;
   if (score) {
     return NextResponse.json(`점수 ${score} post 성공`, {
       status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
     });
   } else {
-    return NextResponse.json("서식이 잘못되었습니다", { status: 200 });
+    return NextResponse.json("서식이 잘못되었습니다", {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
   }
 }
 
@@ -30,8 +49,20 @@ export async function GET(request: NextRequest) {
   if (type === "score") {
     return NextResponse.json(`점수 ${score} get 성공`, {
       status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
     });
   } else {
-    return NextResponse.json("타입이 틀렸습니다", { status: 200 });
+    return NextResponse.json("타입이 틀렸습니다", {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
   }
 }
